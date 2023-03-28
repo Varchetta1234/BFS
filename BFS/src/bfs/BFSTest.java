5f45cd92538a243adb951275bfbed9769d7fe3c9
pbagnes <pbagnes@campus.ungs.edu.ar>
2023-03-27 22:09:48 -0300

    Test BFS grafo completo

--- BFS/src/bfs/BFSTest.java
+++ BFS/src/bfs/BFSTest.java
@@ -48,12 +48,21 @@ public class BFSTest
 	}
 	
 	@Test
-	public void grafoConexoTest() 
+	public void grafoCompletoTest() 
 	{
-		Grafo g = inicializarGrafoInconexo();
-		g.agregarArista(4, 6);
+		Grafo g = inicializarGrafoCompleto();
 		assertTrue(BFS.esConexo(g));
 	}
+	
+	@Test
+	public void alcanzablesGrafoCompletoTest() 
+	{
+		Grafo g = inicializarGrafoCompleto();
+		
+		int[] esperado = {0, 1, 2, 3};
+		Assert.iguales(esperado, BFS.alcanzables(g, 0));
+	}
+
 	
 	@Test
 	public void alcanzablesInconexoTest() 
@@ -74,6 +83,19 @@ public class BFSTest
 		g.agregarArista(2, 4);
 		g.agregarArista(3, 4);
 		g.agregarArista(5, 6);
+		
+		return g;		
+	}
+	
+	private Grafo inicializarGrafoCompleto() 
+	{
+		Grafo g = new Grafo(4);
+		g.agregarArista(0, 1);
+		g.agregarArista(1, 2);
+		g.agregarArista(2, 3);
+		g.agregarArista(0, 3);
+		g.agregarArista(0, 2);
+		g.agregarArista(1, 3);
 		
 		return g;		
 	}
